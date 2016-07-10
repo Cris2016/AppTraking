ca605216629adecaf66021fbfd7f6418145892ff
Victor Aguirre <victor.aguirre417@gmail.com>
2016-07-09 21:52:42 -0600

    Crear modelo de Eventos

---
+++ database/migrations/2016_07_10_034210_create_eventos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('servicio_id')->unsigned();
+            $table->foreign('servicio_id')->references('id')->on('servicios');
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('eventos');
+    }
+}
